7a830e2f18535a6ccbba4262cda407101508183d
slimane-msb <slimanemesbah0@gmail.com>
2026-02-08 22:35:49 +0100

    adding tsp wrapper

--- src/algorithms/TSP/chosen_tsp.ts
+++ src/algorithms/TSP/chosen_tsp.ts
@@ -1,4 +1,15 @@
+import { Graph, TSPResult } from "../../utils/types";
+import { linKernighanTSP } from "./lin_kernighan_heuristic";
+import { heldKarpTSP } from "./held_karp";
+import {astar } from "../shortest path/astar"
 
-
-// <30 : held_karp 
-// else : 
+// Wrapper to choose TSP algorithm based on number of targets
+export function bestTSP(graph: Graph, targets: string[]): TSPResult {
+    if (targets.length < 15) {
+        // Use Held-Karp for small graphs (optimal path)
+        return heldKarpTSP(graph, targets, astar);
+    } else {
+        // Use Lin-Kernighan heuristic for larger graphs (fast, almost optimal path)
+        return linKernighanTSP(graph, targets, astar);
+    }
+}
